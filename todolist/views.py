b3a86eb99e10a3490bd6250a7fb5994046135eb4
Arjun <iamarjunkharal@gmail.com>
2023-07-02 00:57:39 +0545

    FEAT: add of loginMixin

--- todolist/views.py
+++ todolist/views.py
@@ -3,32 +3,34 @@ from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin 
+
 
 from django.urls import reverse_lazy
 
 # Create your views here.
 
-class HomePageView(ListView):
+class HomePageView(LoginRequiredMixin, ListView):
     template_name = 'home.html'
     model = Post
     context_object_name = 'all_task'
 
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     template_name = 'todolist.html'
     model = Post
     context_object_name = 'taskdetail'
 
-class CreatePageView(CreateView):
+class CreatePageView(LoginRequiredMixin, CreateView):
     template_name = 'newtodolist.html'
     fields = ['task', 'body']
     model = Post
 
-class DeletePageView(DeleteView):
+class DeletePageView(LoginRequiredMixin, DeleteView):
     template_name = 'deletetodolist.html'
     model = Post
     success_url = reverse_lazy("home")
 
-class UpdatePageView(UpdateView):
+class UpdatePageView(LoginRequiredMixin, UpdateView):
     template_name = 'updatetodolist.html'
     model  = Post
     fields = ['task', 'body' ] 
